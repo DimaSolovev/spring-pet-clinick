eac124a45406dd42e4f31350cbd8eff9e67f7741
Dima <soloveid87@mail.ru>
2022-07-27 15:43:28 +1000

    index page and controllers

--- pet-clinick-data/src/main/java/com/dima/petclinick/services/VetService.java
+++ pet-clinick-data/src/main/java/com/dima/petclinick/services/VetService.java
@@ -1,11 +1,6 @@
 package com.dima.petclinick.services;
 
-import com.dima.petclinick.model.Owner;
-import com.dima.petclinick.model.Pet;
 import com.dima.petclinick.model.Vet;
-import jdk.vm.ci.meta.Value;
-
-import java.util.Set;
 
 public interface VetService extends CrudService<Vet, Long> {
 
