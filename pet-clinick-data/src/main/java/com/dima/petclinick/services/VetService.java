6bd51a045f7f334540fcbca6bbfb3c8095668e4a
Dima <soloveid87@mail.ru>
2022-07-27 14:38:22 +1000

    refactoring to common interfaces. Closes #11

--- pet-clinick-data/src/main/java/com/dima/petclinick/services/VetService.java
+++ pet-clinick-data/src/main/java/com/dima/petclinick/services/VetService.java
@@ -1,14 +1,12 @@
 package com.dima.petclinick.services;
 
 import com.dima.petclinick.model.Owner;
+import com.dima.petclinick.model.Pet;
 import com.dima.petclinick.model.Vet;
 import jdk.vm.ci.meta.Value;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
